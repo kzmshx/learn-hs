b2e5b1d95d1b11986171f93ffb9a9aec86074014
Kazumasa Hirata <k_hirata@openwork.co.jp>
2025-05-16 00:05:37 +0900

    test(Quadruple): add test cases

--- test/QuadrupleSpec.hs
+++ test/QuadrupleSpec.hs
@@ -4,7 +4,25 @@ import Quadruple (quadruple)
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
-spec = do
-  describe "Quadruple" $ do
-    it "should return 8 when input is 2" $ do
-      quadruple 2 `shouldBe` 8
+spec = describe "quadruple" $ do
+  describe "with Int" $ do
+    let cases =
+          [ (2, 8),
+            (0, 0),
+            (-3, -12)
+          ]
+    let testEach (input, expected) =
+          let desc = "Quadruple(" ++ show input ++ ") = " ++ show expected
+           in it desc $ quadruple (input :: Int) `shouldBe` expected
+    mapM_ testEach cases
+
+  describe "with Double" $ do
+    let cases =
+          [ (2.0, 8.0),
+            (0.0, 0.0),
+            (-3.0, -12.0)
+          ]
+    let testEach (input, expected) =
+          let desc = "Quadruple(" ++ show input ++ ") = " ++ show expected
+           in it desc $ quadruple (input :: Double) `shouldBe` expected
+    mapM_ testEach cases
