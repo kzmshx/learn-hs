179030b844a416f2b5ed4e702dc3fa72f00dcb57
Kazumasa Hirata <k_hirata@openwork.co.jp>
2025-06-02 22:23:29 +0900

    feat(Luhn): add Luhn algorithm implementation and corresponding tests

---
+++ src/Luhn.hs
@@ -0,0 +1,10 @@
+module Luhn (luhn) where
+
+luhnDouble :: Int -> Int
+luhnDouble x
+  | x > 10 = error "luhnDouble: input must not be greater than 10"
+  | x > 4 = 2 * x - 9
+  | otherwise = 2 * x
+
+luhn :: Int -> Int -> Int -> Int -> Bool
+luhn a b c d = (luhnDouble a + b + luhnDouble c + d) `mod` 10 == 0
